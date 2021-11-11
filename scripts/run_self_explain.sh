f98565fe56b314db472af83d1d939fd52b4044be
ostapen <aostapen@andrew.cmu.edu>
2021-11-11 18:18:10 -0500

    Delete run_self_explain.sh

--- scripts/run_self_explain.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export TOKENIZERS_PARALLELISM=false
-python model/run.py --dataset_basedir data/SST-2-XLNet \
-                         --lr 2e-5  --max_epochs 5 \
-                         --gpus 2 \
-                         --concept_store data/SST-2-XLNet/concept_store.pt \
-                         --accelerator ddp
